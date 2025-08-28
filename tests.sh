5862dcf8dd2c0f59288f0fe55bee4120ceb045ec
edgd1er <3601462+edgd1er@users.noreply.github.com>
2025-08-28 03:17:17 +0200

    refactor tests
    rename sockd.conf => danted.conf

--- tests.sh
+++ tests.sh
@@ -1,35 +1,42 @@
 #!/usr/bin/env bash
 
+set -euop pipefail
+
 #vars
 CPSE=compose.yml
 PROXY_HOST="localhost"
-SOCK_PORT=2080 # proxy socks
-HTTP_PORT=2888 # proxy http
-HTTP_PORT=28$(grep -oP '(?<=\- "28)[^:]+' ${CPSE})
-SOCK_PORT=20$(grep -oP '(?<=\- "20)[^:]+' ${CPSE})
-CONFIG_PATH=$(grep -oP '(?<= \- ).+(?=:/config)' ${CPSE})
-SERVICE=transmission
+#HTTP_PORT=28$(grep -oP '(?<=\- "28)[^:]+' ${CPSE})
+#SOCK_PORT=20$(grep -oP '(?<=\- "20)[^:]+' ${CPSE})
+HTTP_PORT=$(grep -oP "(?<=- \")[0-9]{4}(?=:[0-9]{4}\" #http)" ${CPSE})
+SOCK_PORT=$(grep -oP "(?<=- \")[0-9]{4}(?=:[0-9]{4}\" #socks)" ${CPSE})
+SERVICE=$(sed -n '/services:/{n;p}' ${CPSE} | grep -oP '\w+')
 TRANS_PORT=9091
 #Common
 FAILED=0
 INTERVAL=4
-BUILD=1
-TINYLOG=${CONFIG_PATH%/}/log/tinyproxy.log
-DANTELOG=${CONFIG_PATH%/}/log/dante.log
+BUILD=0
+if [[ ${SERVICE} == 'transmission' ]]; then
+  TINYLOG=/config/log/tinyproxy/tinyproxy.log
+  DANTELOG=/config/log/dante.log
+else
+  TINYLOG=/var/log/tinyproxy/tinyproxy.log
+  DANTELOG=/var/log/dante.log
+fi
 
 #Functions
 buildAndWait() {
   echo "Stopping and removing running containers"
   docker compose -f ${CPSE} down -v
+  [[ ${BUILD} -eq 1 ]] && b="--build" || bb=""
   echo "Building and starting image"
-  docker compose -f ${CPSE} up -d --build
+  docker compose -f ${CPSE} up -d ${bb}
+  docker compose -f ${CPSE} exec lynx rm /var/log/{tinyproxy,dante}.log
   echo "Waiting for the container to be up.(every ${INTERVAL} sec)"
   logs=""
-  #  while [ 0 -eq $(echo $logs | grep -c "Initialization Sequence Completed") ]; do
   while [ 0 -eq $(echo $logs | grep -c "exited: start_vpn (exit status 0; expected") ]; do
     logs="$(docker compose -f ${CPSE} logs)"
     sleep ${INTERVAL}
-    ((n += 1))
+    ((n++))
     echo "loop: ${n}: $(docker compose -f ${CPSE} logs | tail -1)"
     [[ ${n} -eq 15 ]] && break || true
   done
@@ -37,7 +44,8 @@ buildAndWait() {
 }
 
 areProxiesPortOpened() {
-  for PORT in ${HTTP_PORT} ${SOCK_PORT} ${TRANS_PORT}; do
+  [[ ${SERVICE} == "transmission" ]] && TM="${TRANS_PORT}" || TM=""
+  for PORT in ${HTTP_PORT} ${SOCK_PORT} ${TM}; do
     msg="Test connection to port ${PORT}: "
     if [ 0 -eq $(echo "" | nc -v -w2 ${PROXY_HOST} ${PORT} 2>&1 | grep -c "] succeeded") ]; then
       msg+=" Failed"
@@ -50,27 +58,35 @@ areProxiesPortOpened() {
 }
 
 testProxies() {
+  FAILED=0
+  if [[ -n $(which nc) ]]; then
+    areProxiesPortOpened
+  fi
   if [[ -f ./tiny_creds ]]; then
     usertiny=$(sed -n '1p' ./tiny_creds)
     passtiny=$(sed -n '2p' ./tiny_creds)
     echo "Getting tinyCreds from file: ${usertiny}:${passtiny}"
     TCREDS="${usertiny}:${passtiny}@"
-    DCREDS=${TCREDS}
+    DCREDS="${TCREDS}"
   else
     usertiny=$(grep -oP "(?<=- TINYUSER=)[^ ]+" ${CPSE})
     passtiny=$(grep -oP "(?<=- TINYPASS=)[^ ]+" ${CPSE})
     echo "Getting tinyCreds from compose: ${usertiny}:${passtiny}"
     TCREDS="${usertiny}:${passtiny}@"
-    DCREDS=${TCREDS}
-
+    DCREDS="${TCREDS}"
   fi
   if [[ -z ${usertiny:-''} ]]; then
     echo "No tinyCreds"
     TCREDS=""
     DCREDS=""
   fi
-  #check http proxy
-  vpnIP=$(curl -4m5 -sx http://${TCREDS}${PROXY_HOST}:${HTTP_PORT} "https://ifconfig.me/ip")
+  declare -i try
+  try=0
+  vpnIP=
+  while [[ -z $vpnIP ]] && [[ try -lt 3 ]]; do
+    vpnIP=$(curl -4m5 -sx http://${TCREDS}${PROXY_HOST}:${HTTP_PORT} "https://ifconfig.me/ip")
+    try+=1
+  done
   if [[ $? -eq 0 ]] && [[ ${myIp} != "${vpnIP}" ]] && [[ ${#vpnIP} -gt 0 ]]; then
     echo "http proxy: IP is ${vpnIP}, mine is ${myIp}"
   else
@@ -79,11 +95,17 @@ testProxies() {
     ((FAILED += 1))
   fi
 
-  #check sock proxy
-  vpnIP=$(curl -4m5 -sx socks5h://${DCREDS}${PROXY_HOST}:${SOCK_PORT} "http://ipv4.lafibre.info/ip.php") || true
+  #check detected ips
+  try=0
+  vpnIP=
+  while [[ -z $vpnIP ]] && [[ try -lt 3 ]]; do
+    vpnIP=$(curl -4m5 -sx socks5h://${DCREDS}${PROXY_HOST}:${SOCK_PORT} "https://ifconfig.me/ip") || true
+    try+=1
+  done
   if [[ $? -eq 0 ]] && [[ ${myIp} != "${vpnIP}" ]] && [[ ${#vpnIP} -gt 0 ]]; then
     echo "socks proxy: IP is ${vpnIP}, mine is ${myIp}"
   else
+    #echo "Error, curl through socks proxy to https://ifconfig.me/ip failed"
     echo "Error, curl through socks proxy to http://ipv4.lafibre.info/ip.php failed"
     echo "or IP (${myIp}) == vpnIP (${vpnIP})"
     ((FAILED += 1))
@@ -94,6 +116,7 @@ testProxies() {
 }
 
 getInterfacesInfo() {
+  docker compose exec ${SERVICE} bash -c "nordvpn version"
   docker compose exec ${SERVICE} bash -c "ip -j a |jq  '.[]|select(.ifname|test(\"wg0|tun|nordlynx\"))|.ifname'"
   itf=$(docker compose -f ${CPSE} exec ${SERVICE} ip -j a)
   echo eth0:$(echo $itf | jq -r '.[] |select(.ifname=="eth0")| .addr_info[].local')
@@ -118,65 +141,107 @@ getTransWebPage() {
 checkOuput() {
   TINY_OUT=$(grep -oP '(?<=\- TINYLOGOUTPUT=)[^ ]+' ${CPSE})
   DANTE_OUT=$(grep -oP '(?<=\- DANTE_LOGOUTPUT=)[^ ]+' ${CPSE})
-  DANTE_RES=$(docker compose -f ${CPSE} exec ${SERVICE} grep -oP "(?<=^logoutput: ).+" /etc/dante.conf)
+  DANTE_RES=$(docker compose -f ${CPSE} exec ${SERVICE} grep -oP "(?<=^logoutput: ).+" /etc/danted.conf 2>/dev/null)
   TINY_RES=$(docker compose -f ${CPSE} exec ${SERVICE} grep -oP "(?<=^LogFile )(?:\")[^\"]+" /etc/tinyproxy/tinyproxy.conf | tr -d '"')
+  TINY_RES=${TINY_RES:-'no log'}
   echo -e "\nOut tiny: ${TINY_OUT}, dante: ${DANTE_OUT}"
   echo "Res tiny: ${TINY_RES}, dante: ${DANTE_RES}"
   echo "Logs tiny: ${TINYLOG}, dante: ${DANTELOG}"
   #dantelog check
   echo "Logs output checks:"
   if [[ ${DANTE_OUT} =~ file ]]; then
-    if [[ ! -f ${DANTELOG} ]]; then
-      echo "ERROR, $DANTELOG not found when $DANTE_OUT == file. config found: ${DANTE_RES}"
+    d=$(($(date +%s) - $(docker compose exec ${SERVICE} date +%s -r ${DANTE_RES})))
+    if [[ ${DANTE_RES} == stdout ]] || [[ 10000 -lt ${d} ]]; then
+      echo "ERROR, $DANTELOG not found when $DANTE_OUT == file. Last access: ${d}, config found: ${DANTE_RES}"
     else
-      echo "OK, $DANTELOG found as expected."
+      echo "OK, $DANTELOG found as expected. Last access: ${d}"
     fi
   else
-    if [[ -f ${DANTELOG} ]]; then
-      echo "ERROR, $DANTELOG found when $DANTE_OUT == stdout. config found: ${DANTE_RES}"
+    #dante config stdout expected
+    if [[ ${DANTE_RES} != "stdout" ]]; then
+      echo "ERROR, $DANTE_OUT is stdout found when $DANTE_RES != stdout. config found: ${DANTE_RES}"
     else
-      echo "OK, $DANTELOG not found as expected."
+      echo "OK, $DANTELOG not found as expected. DANTE config: ${DANTE_RES}"
     fi
   fi
   #tinyproxylog check
   if [[ ${TINY_OUT} =~ file ]]; then
-    if [[ ! -f ${TINYLOG} ]]; then
-      echo "ERROR, $TINYLOG not found when $TINY_OUT == file. config found: ${TINY_RES}"
+    t=$(($(date +%s) - $(docker compose -f ${CPSE} exec ${SERVICE} date +%s -r ${TINY_RES})))
+    if [[ ${TINY_RES} == stdout ]] || [[ 10000 -lt ${t} ]]; then
+      echo "ERROR, $TINYLOG not found when $TINY_OUT == file. Last access: ${t}, config found: ${TINY_RES}"
     else
-      echo "OK, $TINYLOG found as expected."
+      echo "OK, $TINYLOG found as expected. Last access: ${t}."
     fi
   else
-    if [[ -f ${TINYLOG} ]]; then
+    ## TINY_RES should be empty
+    if [[ -n ${TINY_RES} ]]; then
       echo "ERROR, $TINYLOG found when $TINY_OUT == stdout. config found: ${TINY_RES}"
     else
-      echo "OK, $TINYLOG not found as expected."
+      echo "OK, $TINYLOG not found as expected. config found: ${TINY_RES}"
     fi
   fi
 }
 
+checkContainer() {
+  if [[ "localhost" == "${PROXY_HOST}" ]] && [[ 1 -eq ${BUILD} ]]; then
+    buildAndWait
+  fi
+  echo "***************************************************"
+  echo "Testing container"
+  echo "***************************************************"
+  # check returned IP through http and socks proxy
+  testProxies
+  getInterfacesInfo
+  getAliasesOutput
+  if [[ ${SERVICE} == "transmission" ]]; then
+    echo "Getting transmission web page"
+    getTransWebPage
+  fi
+  checkOuput
+  [[ 1 -eq ${BUILD} ]] && docker compose down
+}
+
+usage() {
+  echo "$0: build and test container"
+  echo -e "\t-b\tBuild and test"
+  echo -e "\t-t\tTest a running container"
+  echo -e "\t-u\tTest an ubuntu container (debug nordvpn client)"
+  echo -e "\t-h\tThis help"
+  echo -e "\n\n username password may be saved to tiny_creds for proxy(http/socks), to nordvpn_creds for nordvpn client."
+}
+
 #Main
-[[ -f ${CONFIG_PATH}/log/dante.log ]] && rm -f ${CONFIG_PATH}/log/dante.log || true
-[[ -f ${CONFIG_PATH}/log/tinyproxy.log ]] && rm -f ${CONFIG_PATH}/log/tinyproxy.log || true
-[[ -e /.dockerenv ]] && PROXY_HOST=
-
-#Check ports
-[[ ${1:-''} == "-t" ]] && BUILD=0 || BUILD=1
 [[ -z $(which nc) ]] && echo "No nc found" && exit || true
-
+[[ -e /.dockerenv ]] && PROXY_HOST=
 myIp=$(curl -4m5 -sq https://ifconfig.me/ip)
 
-if [[ "localhost" == "${PROXY_HOST}" ]] && [[ 1 -eq ${BUILD} ]]; then
-  buildAndWait
-fi
-echo "***************************************************"
-echo "Testing container"
-echo "***************************************************"
-# check returned IP through http and socks proxy
-testProxies
-getInterfacesInfo
-getTransWebPage
-getAliasesOutput
-checkOuput
-if [[ "localhost" == "${PROXY_HOST}" ]] && [[ 1 -eq ${BUILD} ]]; then
-  docker compose down
+# Get the options
+while getopts ":bhtu" option; do
+  case ${option} in
+  b)
+    BUILD=1
+    checkContainer
+    ;;
+  h) # display Help
+    usage
+    exit
+    ;;
+  t) # test a running container
+    BUILD=0
+    checkContainer
+    ;;
+  u) # build ubuntu image with nordvpn client
+    ubuntuBuild
+    exit
+    ;;
+  ? | *)
+    echo "Unknown option"
+    usage
+    exit
+    ;;
+  esac
+done
+
+if [ $# -eq 0 ]; then
+  usage
 fi
